7824aae312fa1f82ac28d3f05559863c41cd351e
Toya <hengkongkeaoudong@gmail.com>
2024-01-31 10:58:37 +0700

    added rive animation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:rive/rive.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,28 +30,54 @@ class _MyHomePageState extends State<MyHomePage> {
   bool textScanning = false;
 
   XFile? imageFile;
-  
+
   String scannedText = "";
+
+  StateMachineController? controller;
+
+  SMIInput<bool>? trigSuccess;
+  SMIInput<bool>? trigFail;
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Text Recognition"),
+        title: const Text("Text Recognition"),
       ),
       body: Center(
         child: SingleChildScrollView(
           child: Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                  if(textScanning) const CircularProgressIndicator(),
-                  if(!textScanning && imageFile == null)
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: size.width,
+                    height: 200,
+                    child: RiveAnimation.asset(
+                      "assets/animated_login_character.riv",
+                      stateMachines: const ["Login Machine"],
+                      onInit: (artboard) {
+                        controller = StateMachineController.fromArtboard(
+                            artboard, "Login Machine");
+                        if (controller == null) return;
+
+                        artboard.addController(controller!);
+                        trigSuccess = controller?.findInput("trigSuccess");
+                        trigFail = controller?.findInput("trigFail");
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if (textScanning) const CircularProgressIndicator(),
+                  if (!textScanning && imageFile == null)
                     Container(
-                      width: 300,
-                      height: 300,
+                      width: 250,
+                      height: 250,
                       color: Colors.grey,
                     ),
                   if (imageFile != null) Image.file(File(imageFile!.path)),
@@ -61,24 +88,24 @@ class _MyHomePageState extends State<MyHomePage> {
                         margin: const EdgeInsets.symmetric(horizontal: 5),
                         padding: const EdgeInsets.only(top: 10),
                         child: ElevatedButton(
-                          onPressed: (){
+                          onPressed: () {
                             getImage(ImageSource.gallery);
                           },
                           child: Container(
                             margin: const EdgeInsets.all(5),
                             child: const Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children:[
-                                Icon(
-                                  Icons.image,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Gallery",
-                                  style: TextStyle(fontSize: 13, color: Colors.grey),
-                                )
-                              ]
-                            ),
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(
+                                    Icons.image,
+                                    size: 30,
+                                  ),
+                                  Text(
+                                    "Gallery",
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.grey),
+                                  )
+                                ]),
                           ),
                         ),
                       ),
@@ -86,80 +113,137 @@ class _MyHomePageState extends State<MyHomePage> {
                         margin: const EdgeInsets.symmetric(horizontal: 5),
                         padding: const EdgeInsets.only(top: 10),
                         child: ElevatedButton(
-                          onPressed: (){
+                          onPressed: () {
                             getImage(ImageSource.camera);
                           },
                           child: Container(
                             margin: const EdgeInsets.all(5),
                             child: const Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children:[
-                                Icon(
-                                  Icons.camera_alt,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Camera",
-                                  style: TextStyle(fontSize: 13, color: Colors.grey),
-                                )
-                              ]
-                            ),
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(
+                                    Icons.camera_alt,
+                                    size: 30,
+                                  ),
+                                  Text(
+                                    "Camera",
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.grey),
+                                  )
+                                ]),
                           ),
                         ),
                       )
                     ],
                   ),
-                  const SizedBox(height: 10,),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: MaterialButton(
+                          onPressed: () {
+                            if (trigFail != null) {
+                              trigFail!.change(false);
+                            }
+                            if (trigSuccess == null) return;
+
+                            trigSuccess!.change(true);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.all(5),
+                            child: const Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Happy",
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.grey),
+                                  )
+                                ]),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: MaterialButton(
+                          onPressed: () {
+                            if (trigSuccess != null) {
+                              trigSuccess!.change(false);
+                            }
+                            if (trigFail == null) return;
+
+                            trigFail!.change(true);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.all(5),
+                            child: const Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    "Sad",
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.grey),
+                                  )
+                                ]),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                   Container(
                     child: Text(
                       scannedText,
                       style: TextStyle(fontSize: 20),
                     ),
                   )
-              ],
-              )
-          ),
+                ],
+              )),
         ),
       ),
     );
   }
 
   void getImage(ImageSource source) async {
-    try{
+    try {
       final pickedImage = await ImagePicker().pickImage(source: source);
-      if (pickedImage != null){
+      if (pickedImage != null) {
         textScanning = true;
         imageFile = pickedImage;
-        setState((){
+        setState(() {
           getRecognisedText(pickedImage);
         });
       }
     } catch (e) {
       textScanning = false;
       imageFile = null;
-      setState((){});
+      setState(() {});
       scannedText = "Error Occured";
     }
   }
-  void getRecognisedText(XFile image) async{
+
+  void getRecognisedText(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
     final textDetector = GoogleMlKit.vision.textRecognizer();
     RecognizedText recognizedText = await textDetector.processImage(inputImage);
     await textDetector.close();
     scannedText = "";
-    for(TextBlock block in recognizedText.blocks){
-      for(TextLine line in block.lines){
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
         scannedText = "$scannedText${line.text}\n";
       }
     }
     textScanning = false;
-    setState(() {
-      
-    });
-    
+    setState(() {});
   }
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 }
